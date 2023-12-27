2214fee807b319637dc6e9934a7648db82be68d0
Oleksandr Voitenko <129169030+OlexVoit@users.noreply.github.com>
2023-12-27 16:45:35 +0100

    solution

--- src/main/java/mate/academy/bookshop/dto/user/UserRegistrationRequestDto.java
+++ src/main/java/mate/academy/bookshop/dto/user/UserRegistrationRequestDto.java
@@ -10,18 +10,19 @@ import org.hibernate.validator.constraints.Length;
 @FieldMatch
 public class UserRegistrationRequestDto {
     private static final String NOT_BE_NULL = "Don't be null";
-    private static final String FROM_8_TO_24_CHARACTERS = "Must contain 8 to 24 characters long";
+    private static final String FROM_4_TO_24_CHARACTERS = "Must contain 6 to 24 characters long";
     private static final String INCORRECT_EMAIL_FORMAT = "Incorrect email format";
 
     @Email(message = INCORRECT_EMAIL_FORMAT)
+    @NotBlank(message = NOT_BE_NULL)
     private String email;
 
     @NotBlank(message = NOT_BE_NULL)
-    @Length(min = 8, max = 24, message = FROM_8_TO_24_CHARACTERS)
+    @Length(min = 4, max = 24, message = FROM_4_TO_24_CHARACTERS)
     private String password;
 
     @NotBlank(message = NOT_BE_NULL)
-    @Length(min = 8, max = 24, message = FROM_8_TO_24_CHARACTERS)
+    @Length(min = 4, max = 24, message = FROM_4_TO_24_CHARACTERS)
     private String repeatPassword;
 
     @NotBlank(message = NOT_BE_NULL)
--- src/main/java/mate/academy/bookshop/service/impl/UserServiceImpl.java
+++ src/main/java/mate/academy/bookshop/service/impl/UserServiceImpl.java
@@ -8,6 +8,7 @@ import mate.academy.bookshop.mapper.UserMapper;
 import mate.academy.bookshop.model.User;
 import mate.academy.bookshop.repository.UserRepository;
 import mate.academy.bookshop.service.UserService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,6 +17,7 @@ public class UserServiceImpl implements UserService {
     private static final String CAN_NOT_REGISTER_USER_BY_EMAIL = "Can't register a user by email:";
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public UserResponseDto register(UserRegistrationRequestDto requestDto)
@@ -24,6 +26,7 @@ public class UserServiceImpl implements UserService {
             throw new RegistrationException(CAN_NOT_REGISTER_USER_BY_EMAIL + requestDto.getEmail());
         }
         User userSave = userMapper.toModel(requestDto);
+        userSave.setPassword(passwordEncoder.encode(requestDto.getPassword()));
         return userMapper.toDto(userRepository.save(userSave));
     }
 }

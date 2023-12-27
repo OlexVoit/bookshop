d6e97271f831bf882d364cbc29bd3a86553bbea4
Oleksandr Voitenko <129169030+OlexVoit@users.noreply.github.com>
2023-12-27 12:39:43 +0100

    added entity role and create table

---
+++ src/main/java/mate/academy/bookshop/model/Role.java
@@ -0,0 +1,22 @@
+package mate.academy.bookshop.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "role_name", nullable = false, unique = true)
+    private RoleName roleName;
+
+    public enum RoleName {
+        ADMIN, USER
+    }
+}
+
+

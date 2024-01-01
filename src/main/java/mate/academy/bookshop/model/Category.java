a038a4f83421b096629eb641381332b31a44d6c9
Oleksandr Voitenko <129169030+OlexVoit@users.noreply.github.com>
2024-01-01 13:30:47 +0100

    + category entity

---
+++ src/main/java/mate/academy/bookshop/model/Category.java
@@ -0,0 +1,28 @@
+package mate.academy.bookshop.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Entity
+@SQLDelete(sql = "UPDATE books SET is_deleted = true WHERE id=?")
+@SQLRestriction("is_deleted=false")
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "is_deleted", nullable = false)
+    private boolean isDeleted = false;
+}

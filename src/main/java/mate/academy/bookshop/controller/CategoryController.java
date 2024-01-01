cd006e5fa3dcfa8ac9c9bdd9f52b1cc67dbae228
Oleksandr Voitenko <129169030+OlexVoit@users.noreply.github.com>
2024-01-01 20:46:01 +0100

    + updated the CategoryController

--- src/main/java/mate/academy/bookshop/controller/CategoryController.java
+++ src/main/java/mate/academy/bookshop/controller/CategoryController.java
@@ -4,11 +4,14 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import mate.academy.bookshop.dto.book.BookDto;
 import mate.academy.bookshop.dto.category.CategoryResponseDto;
 import mate.academy.bookshop.dto.category.CreateCategoryRequestDto;
+import mate.academy.bookshop.service.BookService;
 import mate.academy.bookshop.service.CategoryService;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,15 +22,18 @@ import java.util.List;
 @RequestMapping(value = "/api/categories")
 public class CategoryController {
     private final CategoryService categoryService;
+    private final BookService bookService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @Operation(summary = "Create category",
             description = "Only the administrator can create a category")
     public CategoryResponseDto createCategory(@RequestBody @Valid CreateCategoryRequestDto requestDto) {
         return categoryService.createCategory(requestDto);
     }
 
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
     @GetMapping
     @Operation(summary = "Get categories",
             description = "The administrator and user can get "
@@ -36,17 +42,23 @@ public class CategoryController {
         return categoryService.getAll(pageable);
     }
 
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
     @GetMapping("/{id}")
-    @Operation(summary = "Get category by ID")
+    @Operation(summary = "Get category by ID",
+            description = "The administrator and user can find a category by id")
     public CategoryResponseDto getCategoryById(@PathVariable Long id) {
         return categoryService.getCategoryById(id);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @PutMapping("/{id}")
+    @Operation(summary ="Update category",
+            description = "Only the administrator can change the category")
     public CategoryResponseDto updateCategory(@PathVariable Long id, @RequestBody CreateCategoryRequestDto requestDto) {
         return categoryService.updateCategory(id, requestDto);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @DeleteMapping("/{id}")
     @Operation(summary = "Delete a category",
             description = "Only the administrator can delete a category by ID")
@@ -54,5 +66,8 @@ public class CategoryController {
         categoryService.deleteCategoryById(id);
     }
 
-
+    @GetMapping("/{id}/books")
+    public List<BookDto> getBooksByCategoryId(@PathVariable Long id) {
+        return bookService.getBooksByCategoryId(id);
+    }
 }

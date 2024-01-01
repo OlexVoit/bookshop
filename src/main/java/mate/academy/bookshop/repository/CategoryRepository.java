f4963c30be69bd75f46695c8e3dcdce7edbdb33a
Oleksandr Voitenko <129169030+OlexVoit@users.noreply.github.com>
2024-01-01 17:22:20 +0100

    + added CategoryRepository

--- src/main/java/mate/academy/bookshop/repository/CategoryRepository.java
+++ src/main/java/mate/academy/bookshop/repository/CategoryRepository.java
@@ -3,6 +3,6 @@ package mate.academy.bookshop.repository;
 import mate.academy.bookshop.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoriesRepository extends JpaRepository<Category, Long> {
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 
 }

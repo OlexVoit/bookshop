eeb441044ab0006547672507eba060c70d134ce5
Oleksandr Voitenko <129169030+OlexVoit@users.noreply.github.com>
2024-01-03 22:55:57 +0100

    +- minor bugs fixed (part 3)

--- src/main/java/mate/academy/bookshop/exception/UnauthorizedOperationException.java
+++ src/main/java/mate/academy/bookshop/exception/UnauthorizedOperationException.java
@@ -1,6 +1,6 @@
 package mate.academy.bookshop.exception;
 
-public class UnauthorizedOperationException extends RuntimeException{
+public class UnauthorizedOperationException extends RuntimeException {
     public UnauthorizedOperationException(String message) {
         super(message);
     }

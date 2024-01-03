15aac20f452d31ce2d31c2671d7cf083a2e84b8b
Oleksandr Voitenko <129169030+OlexVoit@users.noreply.github.com>
2024-01-03 22:58:46 +0100

    +- added to ShoppingCartServiceImpl two methods (getShoppingCartByUser\deleteShoppingCartById)

--- src/main/java/mate/academy/bookshop/service/ShoppingCartService.java
+++ src/main/java/mate/academy/bookshop/service/ShoppingCartService.java
@@ -4,5 +4,10 @@ import mate.academy.bookshop.dto.shoppingcart.AddToCartRequestDto;
 import mate.academy.bookshop.dto.shoppingcart.ShoppingCartDto;
 
 public interface ShoppingCartService {
-    ShoppingCartDto addToCart (AddToCartRequestDto requestDto, Long userID);
+    ShoppingCartDto addToCart(AddToCartRequestDto requestDto, Long userID);
+
+    ShoppingCartDto getShoppingCartByUser(Long userId);
+
+    void deleteShoppingCartById(Long cartId, Long userId);
+
 }
--- src/main/java/mate/academy/bookshop/service/impl/ShoppingCartServiceImpl.java
+++ src/main/java/mate/academy/bookshop/service/impl/ShoppingCartServiceImpl.java
@@ -1,9 +1,12 @@
 package mate.academy.bookshop.service.impl;
 
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import mate.academy.bookshop.dto.shoppingcart.AddToCartRequestDto;
 import mate.academy.bookshop.dto.shoppingcart.ShoppingCartDto;
 import mate.academy.bookshop.exception.EntityNotFoundException;
+import mate.academy.bookshop.exception.ResourceNotFoundException;
+import mate.academy.bookshop.exception.UnauthorizedOperationException;
 import mate.academy.bookshop.mapper.ShoppingCartMapper;
 import mate.academy.bookshop.model.Book;
 import mate.academy.bookshop.model.CartItem;
@@ -17,7 +20,6 @@ import mate.academy.bookshop.service.ShoppingCartService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-
 @Service
 @RequiredArgsConstructor
 public class ShoppingCartServiceImpl implements ShoppingCartService {
@@ -26,6 +28,30 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
     private final UserRepository userRepository;
     private final CartItemRepository cartItemRepository;
     private final ShoppingCartMapper shoppingCartMapper;
+
+    @Override
+    public ShoppingCartDto getShoppingCartByUser(Long userId) {
+        ShoppingCart shoppingCart = shoppingCartRepository
+                .findByUserId(userId).orElseThrow(() ->
+                        new ResourceNotFoundException("User does not have a shopping cart"
+                                + userId));
+        return shoppingCartMapper.toDto(shoppingCart);
+    }
+
+    @Override
+    public void deleteShoppingCartById(Long cartId, Long userId) {
+        shoppingCartRepository.findById(cartId)
+                .ifPresent(shoppingCarts -> {
+                    if (Objects.equals(shoppingCarts.getUser().getId(), userId)) {
+                        shoppingCartRepository.deleteById(cartId);
+                    } else {
+                        throw new UnauthorizedOperationException("User is not authorized "
+                                + "to delete this shopping cart");
+                    }
+                });
+        throw new ResourceNotFoundException("The Shopping cart was not found or deleted. Id: "
+                + cartId);
+    }
 
     @Transactional
     @Override
@@ -39,12 +65,13 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
                         new EntityNotFoundException("Can't find the user by id: "
                                 + userId));
 
-       ShoppingCart shoppingCartFromDb = shoppingCartRepository.findByUser_Id(userId).orElseGet(() -> {
-            ShoppingCart shoppingCart = new ShoppingCart();
-            shoppingCart.setUser(user);
-            shoppingCartRepository.save(shoppingCart);
-            return shoppingCart;
-        });
+        ShoppingCart shoppingCartFromDb =
+                shoppingCartRepository.findByUser_Id(userId).orElseGet(() -> {
+                    ShoppingCart shoppingCart = new ShoppingCart();
+                    shoppingCart.setUser(user);
+                    shoppingCartRepository.save(shoppingCart);
+                    return shoppingCart;
+                });
 
         CartItem cartItem = new CartItem();
         cartItem.setQuantity(requestDto.getQuantity());

f3f35200476fbd3305017d988902827b0ca6539d
Oleksandr Voitenko <129169030+OlexVoit@users.noreply.github.com>
2024-01-03 22:57:18 +0100

    +- added ShoppingCartDto

---
+++ src/main/java/mate/academy/bookshop/dto/shoppingcart/ShoppingCartDto.java
@@ -0,0 +1,11 @@
+package mate.academy.bookshop.dto.shoppingcart;
+
+import java.util.Set;
+import lombok.Data;
+
+@Data
+public class ShoppingCartDto {
+    private Long id;
+    private Long userId;
+    private Set<CartItemDto> cartItems;
+}
